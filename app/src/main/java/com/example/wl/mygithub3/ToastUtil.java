2f44295433743a36d83e228ceab5f7fb88ce148b
wl <790447091@qq.com>
2018-08-25 12:48:12 +0800

    增加toast的工具类

---
+++ app/src/main/java/com/example/wl/mygithub3/ToastUtil.java
@@ -0,0 +1,11 @@
+package com.example.wl.mygithub3;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtil {
+
+    public static void showToast(Context context){
+        Toast.makeText(context,"sucess",Toast.LENGTH_LONG).show();
+    }
+}
